7b5f64255d521fc88b935c4e66e70cf4892e07c1
Javier Torrez <javierctorrez@gmail.com>
2017-07-04 15:05:28 -0400

    implemented algorithm to return shortest path distance from a source node to a destination node

--- src/DijkstrasAlgo.java
+++ src/DijkstrasAlgo.java
@@ -59,4 +59,64 @@ public class DijkstrasAlgo {
 		
 		return cloud; // only includes reachable vertices
 	}
+	
+	/* Computes shortest-path distances from source vertex to a destination vertex */
+	public static <V> int shortestPathLength(Graph<V, Integer> g, Vertex<V> src, Vertex<V> dest){
+	
+		// d.get(v) is upper bound on distance from src to v 
+		Map<Vertex<V>, Integer> d = new ProbeHashMap<>();
+		
+		// map reachable v to its final d value
+		ProbeHashMap<Vertex<V>, Integer> visited = new ProbeHashMap();
+		
+		// pq will have vertices as values, with d.get(v) as key 
+		HeapAdaptablePriorityQueue<Integer, Vertex<V>> pq = new HeapAdaptablePriorityQueue<>();
+		
+		// maps from vertex to its pq locator
+		Map<Vertex<V>, Entry<Integer, Vertex<V>>> pqTokens = new ProbeHashMap<>();
+		
+		// for each vertex v of the graph, add an entry to the priority queue, with
+		// the source having distance 0 and all others having infinite distance
+		for (Vertex<V> v : g.vertices()){
+			if(v == src){
+				d.put(v, 0);
+			} else {
+				d.put(v, Integer.MAX_VALUE);
+			}
+			
+			pqTokens.put(v, pq.insert(d.get(v), v));		// save entry for future updates
+		}
+		
+		// now begin adding reachable vertices to the cloud 
+		while(!pq.isEmpty()){
+			Entry<Integer, Vertex<V>> entry = pq.removeMin();
+			if(entry != dest){
+				int key = entry.getKey();
+				Vertex<V> u = entry.getValue();
+				
+				visited.put(u, key);								// this is actual distance to u
+				pqTokens.remove(u);								    // u is no longer in pq
+				
+				for(Edge<Integer> e : g.outgoingEdges(u)){
+					Vertex<V> v = g.opposite(u, e);
+					if(visited.get(v) == null){
+						// perform relaxation step n edge (u,v) 
+						int edgeWeight = e.getElement();
+						if (d.get(u) + edgeWeight < d.get(v)){					// better path to v?
+							d.put(v, d.get(u) + edgeWeight);					// update the distance
+							pq.replaceKey(pqTokens.get(v), d.get(v));	     	// update the pq entry
+						}
+					}
+				}
+			} else{
+				int key = entry.getKey();
+				Vertex<V> u = entry.getValue();
+				
+				visited.put(u, key);
+				break;
+			}
+		}
+		
+		return visited.get(dest); // returns shortest path to destination node
+	}
 }
