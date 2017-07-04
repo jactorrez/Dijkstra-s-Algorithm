88a109a8983a58b2570873ef30dc57a3c189eb11
Javier Torrez <javierctorrez@gmail.com>
2017-07-04 15:41:09 -0400

    implemented algorithm to construct shortest-path tree from a given distance map (calculated using dijkstras algorithm)

--- src/DijkstrasAlgo.java
+++ src/DijkstrasAlgo.java
@@ -119,4 +119,25 @@ public class DijkstrasAlgo {
 		
 		return visited.get(dest); // returns shortest path to destination node
 	}
+	
+	/* Reconstructs a shortest-path tree rooted as vertex s, given distance map d.
+	 * The tree is represented as a map from each reachable vertex v (other than s)
+	 * to the edge  e = (u,v) that is used to reach v from its parent u in the tree 
+	 */
+	
+	public static <V> Map<Vertex<V>, Edge<Integer>> spTree(Graph<V, Integer> g, Vertex<V> s, Map<Vertex<V>, Integer> d){
+		Map<Vertex<V>, Edge<Integer>> tree = new ProbeHashMap<>();
+		for(Vertex<V> v : d.keySet()){
+			if (v != s){
+				for (Edge<Integer> e : g.incomingEdges(v)){		// consider incoming edges
+					Vertex<V> u = g.opposite(v, e);
+					int edgeWeight = e.getElement();
+					if(d.get(v) == d.get(u) + edgeWeight){
+						tree.put(v, e);						   // edge to reach v has been found
+					}
+				}
+			}
+		}
+		return tree;
+	}
 }
